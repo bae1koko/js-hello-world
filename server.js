64d5ba01fe984b2bffaebcd4705159a0f7096181
bae1koko <bae1kokox@gmail.com>
2022-08-27 04:46:07 +0000

    Update more messsage chikaboo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h5>สวัสดีจ้าสาวๆ</h5')
+  return res.send('<h5>1234สวัสดีจ้าสาวๆ</h5')
 })
 
 const PORT = process.env.PORT || 3000
