67cdc2fd53d18e10dfc207a7165e18ab7c859115
bae1koko <bae1kokox@gmail.com>
2022-08-27 04:33:32 +0000

    Upd ate messsage asdas jkdsajldas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h5>สวัสดีเมืองเชียงใหม่</h5')
+  return res.send('<h5>สวัสดีจ้าสาวๆ</h5')
 })
 
 const PORT = process.env.PORT || 3000
